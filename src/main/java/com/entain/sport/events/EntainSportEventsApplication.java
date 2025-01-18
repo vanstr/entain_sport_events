1f3c5f1da00f89cb0fc8d7ff1496c9c8c374e35c
Ivan <mihnenok@gmail.com>
2025-01-18 22:03:39 +0100

    Spring initializer

---
+++ src/main/java/com/entain/sport/events/EntainSportEventsApplication.java
@@ -0,0 +1,13 @@
+package com.entain.sport.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntainSportEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntainSportEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/entain/sport/events/EntainSportEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entain.sport.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntainSportEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
